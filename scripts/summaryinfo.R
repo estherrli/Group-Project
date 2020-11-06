379c07710b1d9bb438e08e1ce851ddb8e22ba2de
meganluu <72280727+meganluu@users.noreply.github.com>
2020-11-05 22:59:37 -0800

    summary info edits

--- scripts/summaryinfo.R
+++ scripts/summaryinfo.R
@@ -1,9 +1,4 @@
-# Most recent or oldest movie added to Netflix
-# Average duration of movie
-# Year with most movies released
-# Most popular country
-# Most common type of movie
-# Ratings
+
 
 library(tidyverse)
 library(stringr)

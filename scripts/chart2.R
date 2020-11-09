92c4699ffae6f2bae5eef858e1494c4b3b57c2f6
cchung5063 <69750589+cchung5063@users.noreply.github.com>
2020-11-08 16:14:20 -0800

    chart 2 finished

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -1,1 +1,13 @@
-# Cynthia: Pie chart Movies vs TV Shows
+# Cynthia: Bar Chart Movies vs TV Shows
+netflix <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/netflix_titles.csv")
+
+# Code for chart
+install.packages("ggplot2")
+install.packages("RColorBrewer")
+library(ggplot2)
+library(RColorBrewer)
+
+ggplot(netflix, aes(x = factor(type))) +
+  geom_bar(width = 0.5, fill = "skyblue1") +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")
+

7c530c4fa4538cfc257f9f3eabf1b13a34f48174
ESTHER LI <eli0107@uw.edu>
2020-11-08 23:08:19 -0800

    style changes

--- scripts/aggregatetable.R
+++ scripts/aggregatetable.R
@@ -1,27 +1,14 @@
-#The second file you should save in your scripts/ directory should create a table of aggregate information about it. 
-#It must perform a groupby operation to show a dimension of the dataset as grouped by a particular feature (column). 
-#We expect the included table to:
-#- Have well formatted (i.e., human readable) column names (so you'll probably have to change them)
-# Only contain relevant information (i.e., only select some columns of interest)
-# Be intentionally sorted in a meaningful way
-# Round any quantitative values so they are displayed in a manner that isn't distracting
-# When you display the table in your index.Rmd file, you must also describe why you included the table, 
-#and what information it reveals.
-
-#group by year
+# group by year
 
 library(tidyverse)
-library(stringr)
-
 netflix_titles <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/netflix_titles.csv")
 
-by_year <- netflix_titles %>% 
+by_year <- netflix_titles %>%
   mutate(type_media = if_else(type == "Movie", 1, 0)) %>%
-  group_by(release_year) %>% 
+  group_by(release_year) %>%
   summarise(
     number_released_in_year = n(),
     num_movies = sum(type_media),
     num_tvshows = number_released_in_year - num_movies,
     movie_tvshow_difference = num_movies - num_tvshows
   )
-
--- scripts/chart1.R
+++ scripts/chart1.R
@@ -4,21 +4,21 @@ library(ggplot2)
 library(RColorBrewer)
 netflix_titles <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/netflix_titles.csv")
 
-each_year <- netflix_titles %>% 
+each_year <- netflix_titles %>%
   mutate(type_media = if_else(type == "Movie", 1, 0)) %>%
-  group_by(release_year) %>% 
+  group_by(release_year) %>%
   summarise(
     number_released_in_year = n(),
     num_movies = sum(type_media),
     num_tvshows = number_released_in_year - num_movies,
     movie_tvshow_difference = num_movies - num_tvshows
-  ) %>% 
+  ) %>%
   select(release_year, num_movies, num_tvshows) %>%
-  gather(key = type_media, value = number_released_in_year, -release_year) # all columns except `state`
-
+  gather(key = type_media, value = number_released_in_year,
+         -release_year)
 
 ggplot(data = each_year) +
   geom_col(
-    mapping = aes(x = release_year, y = number_released_in_year, 
+    mapping = aes(x = release_year, y = number_released_in_year,
                   fill = type_media),
     )

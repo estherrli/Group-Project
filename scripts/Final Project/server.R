db042d8e96392c4ac1670c0604f1b20b40de49f0
meganluu <72280727+meganluu@users.noreply.github.com>
2020-12-07 20:19:32 -0800

    final app

--- scripts/Final Project/server.R
+++ scripts/Final Project/server.R
@@ -5,95 +5,100 @@ library(tidyverse)
 library(plotly)
 library(stringr)
 
-#chart 1 data
+# chart 1 data
 netflix_titles <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/netflix_titles.csv")
-netflix_titles <- netflix_titles %>% 
-    separate(listed_in, c("genre", "genre2", "genre3"), sep = ",") %>% 
-    mutate(year_added = str_sub(netflix_titles$date_added, start = -4))
-movie_genres <- netflix_titles %>% 
-    filter(type == "Movie") %>% 
-    pull(genre) %>% 
-    unique()
+netflix_titles <- netflix_titles %>%
+  separate(listed_in, c("genre", "genre2", "genre3"), sep = ",") %>%
+  mutate(year_added = str_sub(netflix_titles$date_added, start = -4))
+movie_genres <- netflix_titles %>%
+  filter(type == "Movie") %>%
+  pull(genre) %>%
+  unique()
 
-#chart 2 data
+# chart 2 data
 data_tab2 <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/MoviesOnStreamingPlatforms_updated.csv")
 
-tab2_columns <- data_tab2 %>% 
-    filter(Year == max(Year)) %>% 
-    separate(Genres, c("Genre", "Genre2", "Genre3"), sep = ",") %>% 
-    select(Age, Runtime, Genre, IMDb, Rotten.Tomatoes, Title)
+tab2_columns <- data_tab2 %>%
+  filter(Year == max(Year)) %>%
+  separate(Genres, c("Genre", "Genre2", "Genre3"), sep = ",") %>%
+  select(Age, Runtime, Genre, IMDb, Rotten.Tomatoes, Title)
 
-tab2_columns$Rotten.Tomatoes <- gsub( "%", "", as.character(tab2_columns$Rotten.Tomatoes))
+tab2_columns$Rotten.Tomatoes <- gsub("%", "",
+                                     as.character(tab2_columns$Rotten.Tomatoes))
 
 tab2_columns$Rotten.Tomatoes <- as.numeric(tab2_columns$Rotten.Tomatoes)
 
 tab2_select_values <- colnames(tab2_columns)
 
-#chart 3 data
+# chart 3 data
 netflix_stock <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/NFLX.csv")
 y_options <- colnames(netflix_stock)[2:7]
 
 
 server <- function(input, output) {
-    # Page 1 chart
-    output$chart1 <- renderPlotly({
-        # Download datafiles
-        filtered_data <- netflix_titles %>% 
-            filter(type == "Movie") %>% 
-            filter(genre == input$chart1_genre) %>% 
-            group_by(year_added, na.rm = T) %>% 
-            summarize(count = n(), na.rm = T)
-        title <- paste0(
-            input$chart1_genre, " Added to Netflix By Year"
-        )
-        plot <- ggplot(data = filtered_data) + 
-            geom_col(mapping = aes(
-                x = year_added,
-                y = count),
-                fill = input$chart1_color)+
-            labs(
-                x = "Year",
-                y = "Number of Movies Added",
-                title = title
-            )
-        ggplotly(plot, tooltip = "y")
-    })
-    # Page 2 Chart
-    output$chart2 <- renderPlotly({
-        title <- paste0(input$y_var, " Rating ", " v.s. ", input$x_var)
-        p <- ggplot(tab2_columns, aes_string(x = input$x_var, y = input$y_var,
-                                             Title = "Title")) +
-            geom_point(stat = 'identity', color = input$color) +
-            labs(x = input$x_var, y = input$y_var, title = title) +
-            theme(axis.text.x=element_text(angle =- 90, vjust = 0.5))
-        ggplotly(p, tooltip = c("x", "y", "Title"))
-    })
-    # Page 3 Chart
-    output$chart3 <- renderPlotly({
-        
-        # Store the title of the graph in a variable indicating the x/y variables
-        title <- paste0("Netflix Stock Prices: ",
-                        "Year vs ", input$chart3_y_var)
-        
-        # separate date into three columns (year, month, day) calculate average
-        # for each variable by year
-        stock_plot <- netflix_stock %>%
-            mutate(year = lubridate::year(Date), 
-                   month = lubridate::month(Date), 
-                   day = lubridate::day(Date))
-        
-        # Create ggplot scatter
-        p <- ggplot(stock_plot) +
-            geom_point(mapping = aes_string(x = "year", y = input$chart3_y_var),
-                       color = input$chart3_color) +
-            labs(x = "Year", y = input$chart3_y_var, title = title)
-        p
-    })
-    
+  # Page 1 chart
+  output$chart1 <- renderPlotly({
+    # Download datafiles
+    filtered_data <- netflix_titles %>%
+      filter(type == "Movie") %>%
+      filter(genre == input$chart1_genre) %>%
+      group_by(year_added, na.rm = T) %>%
+      summarize(count = n(), na.rm = T)
+    title <- paste0(
+      input$chart1_genre, " Added to Netflix By Year"
+    )
+    plot <- ggplot(data = filtered_data) +
+      geom_col(
+        mapping = aes(
+          x = year_added,
+          y = count
+        ),
+        fill = input$chart1_color
+      ) +
+      labs(
+        x = "Year",
+        y = "Number of Movies Added",
+        title = title
+      )
+    ggplotly(plot, tooltip = "y")
+  })
+  # Page 2 Chart
+  output$chart2 <- renderPlotly({
+    title <- paste0(input$y_var, " Rating ", " v.s. ", input$x_var)
+    p <- ggplot(tab2_columns, aes_string(
+      x = input$x_var, y = input$y_var,
+      Title = "Title"
+    )) +
+      geom_point(stat = "identity", color = input$color) +
+      labs(x = input$x_var, y = input$y_var, title = title) +
+      theme(axis.text.x = element_text(angle = -90, vjust = 0.5))
+    ggplotly(p, tooltip = c("x", "y", "Title"))
+  })
+  # Page 3 Chart
+  output$chart3 <- renderPlotly({
+
+    # Store the title of the graph in a variable indicating the x/y variables
+    title <- paste0(
+      "Netflix Stock Prices: ",
+      "Year vs ", input$chart3_y_var
+    )
+
+    # separate date into three columns (year, month, day) calculate average
+    # for each variable by year
+    stock_plot <- netflix_stock %>%
+      mutate(
+        year = lubridate::year(Date),
+        month = lubridate::month(Date),
+        day = lubridate::day(Date)
+      )
+
+    # Create ggplot scatter
+    p <- ggplot(stock_plot) +
+      geom_point(
+        mapping = aes_string(x = "year", y = input$chart3_y_var),
+        color = input$chart3_color
+      ) +
+      labs(x = "Year", y = input$chart3_y_var, title = title)
+    p
+  })
 }
-
-
-
-
-
-
--- scripts/Final Project/ui.R
+++ scripts/Final Project/ui.R
@@ -7,55 +7,94 @@ library(stringr)
 
 ## INTRO PAGE ##
 intro_page <- tabPanel(
-    "Introduction",
-    h1("Introduction"),
-    p("intro lol")
+  tags$h4("Introduction"),
+  h1("Introduction"),
+  p("In the year of 2020, COVID-19 caused a nationwide lockdown
+    that led people to stay at home without access to the outside world. With
+    this extended time at home, many people turned to streaming platforms such
+    as Netflix for entertainment. Because of this widened use of Netflix, we
+    decided to explore and learn more about the platform that we have all been
+    using extensively during the past few months. Therefore, for our final
+    project, we decided to analyze trends in datasets
+      containing information on Netflix."),
+  p("Our first major question was: How have
+      the different movie genres on Netflix changed over time, and what can this
+      tell us about trends in the movie industry? To answer this,
+      we found a dataset called 'Netflix Titles', which includes data on
+      directors, casts, genres, duration, and release years for Netflix movies.
+      Using this dataset, we were able to create an interactive bar chart to
+      display patterns in the different movie genres on Netflix."),
+  p("Our second major question was: what are some main components that
+      determine the quality of a movie and therefore gain a large audience?
+      To find the answer to this, we decided to explore ratings of movies and
+      how specific variables affected them. To do so, we found a dataset,
+      called ‘Movies On Streaming Platforms’, that compared individual movies’
+      age group, runtime, and genre against IMDb ratings and Rotten Tomato
+      ratings. Using a scatterplot, we plotted the variables against the ratings
+      to find if any patterns or correlations were revealed."),
+  p("Our third major question was how Netflix's value changed and can it be
+      attributed to overall media consumption? Our second graph analyzes data
+      regarding Netlfix stock prices since it first went public in 2002. The
+      data contains information on daily opening, closing, maximum, minimum,
+      volume of stock traded, and closing prices adjusted with split and
+      dividend multipliers. Our interactive scatter plot allows you to analyze
+      each of these variables individually from year to year and
+      illustrate patterns."),
+  tags$img(src = "https://specials-images.forbesimg.com/imageserve/5e9ddacf8c2caa0006e6f7a5/960x0.jpg?fit=scale")
 )
+
+
 
 
 ## PAGE 1 ##
 
 # Download datafiles
 netflix_titles <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/netflix_titles.csv")
-netflix_titles <- netflix_titles %>% 
-  separate(listed_in, c("genre", "genre2", "genre3"), sep = ",") %>% 
+netflix_titles <- netflix_titles %>%
+  separate(listed_in, c("genre", "genre2", "genre3"), sep = ",") %>%
   mutate(year_added = str_sub(netflix_titles$date_added, start = -4))
 
 # Get vector of movie genres to use for widget
-movie_genres <- netflix_titles %>% 
-    filter(type == "Movie") %>% 
-    pull(genre) %>% 
-    unique()
+movie_genres <- netflix_titles %>%
+  filter(type == "Movie") %>%
+  pull(genre) %>%
+  unique()
 
 chart1_genre <- selectInput(
-    "chart1_genre",
-    label = h4("Select a movie genre to display: "),
-    choices = movie_genres)
+  "chart1_genre",
+  label = h4("Select a movie genre to display: "),
+  choices = movie_genres
+)
 
 
 chart1_color <- radioButtons(
-    "chart1_color",
-    label = h4("Select a bar color: "),
-    choices = list("Red" = "tomato1", 
-                   "Blue"= "royalblue2", 
-                   "Green" = "seagreen4", 
-                   "Purple" = "mediumpurple1")
+  "chart1_color",
+  label = h4("Select a bar color: "),
+  choices = list(
+    "Red" = "tomato1",
+    "Blue" = "royalblue2",
+    "Green" = "seagreen4",
+    "Purple" = "mediumpurple1"
+  )
 )
 
 page_one <- tabPanel(
-    "Netflix Movie Genres",
-    h1("Netflix Movie Genre Exploration"),
-    sidebarLayout(
-        sidebarPanel(p(chart1_genre), p(chart1_color)),
-        mainPanel(p(plotlyOutput("chart1", height = "700px")))
-    ),
-    p("This interactive bar chart displays the number of movies added to Netflix
-    each year for a chosen genre. The chosen genre can be altered using the
-    first widget. The second widget allows users to change the color of the
-    bars. Using this chart, we can explore the total counts of each movie
-    genre each year -- for example, we can see that the most Children & 
-    Family movies were added in 2019, while the most Stand-Up Comedies were
-    added in 2018.")
+  tags$h4("Netflix Movie Genres"),
+  h1("Netflix Movie Genre Exploration"),
+  sidebarLayout(
+    sidebarPanel(p(chart1_genre), p(chart1_color)),
+    mainPanel(p(plotlyOutput("chart1", height = "700px")),
+              p("This interactive bar chart displays the number of movies added
+              to Netflix each year for a chosen genre. The chosen genre can be
+              altered using the first widget. The second widget allows users to
+              change the color of the bars. Using this chart, we can explore the
+              total counts of each movie genre each year -- for example, we can
+              see that the most Children & Family movies were added in 2019,
+              while the most Stand-Up Comedies were
+              added in 2018.")
+              )
+  ),
+
 )
 
 
@@ -63,12 +102,12 @@ page_one <- tabPanel(
 
 data_tab2 <- read.csv("https://raw.githubusercontent.com/estherrli/Group-Project/main/data/MoviesOnStreamingPlatforms_updated.csv")
 
-tab2_columns <- data_tab2 %>% 
-    filter(Year == max(Year)) %>% 
-    separate(Genres, c("Genre", "Genre2", "Genre3"), sep = ",") %>% 
-    select(Age, Runtime, Genre, IMDb, Rotten.Tomatoes, Title) 
+tab2_columns <- data_tab2 %>%
+  filter(Year == max(Year)) %>%
+  separate(Genres, c("Genre", "Genre2", "Genre3"), sep = ",") %>%
+  select(Age, Runtime, Genre, IMDb, Rotten.Tomatoes, Title)
 
-tab2_columns$Rotten.Tomatoes <- gsub( "%", "", as.character(tab2_columns$Rotten.Tomatoes))
+tab2_columns$Rotten.Tomatoes <- gsub("%", "", as.character(tab2_columns$Rotten.Tomatoes))
 
 tab2_columns$Rotten.Tomatoes <- as.numeric(tab2_columns$Rotten.Tomatoes)
 
@@ -77,60 +116,62 @@ tab2_select_values <- colnames(tab2_columns)
 
 
 page_two <- tabPanel(
-    "Movie Ratings",
-    h1("Comparing Ratings to Specific Variables in 2020"),
-    sidebarLayout(
-        sidebarPanel(
-            x_input <- selectInput(
-                "x_var",
-                label = "X Variable",
-                choices = list(
-                    tab2_select_values[1],
-                    tab2_select_values[2], 
-                    tab2_select_values[3] 
-                )
-            ),
-            y_input <- selectInput(
-                "y_var",
-                label = "Rating Type",
-                choices = list(
-                    tab2_select_values[4],
-                    tab2_select_values[5]
-                )
-            ),
-            color_input <- selectInput(
-                "color",
-                label = "Color",
-                choices =  list("Red" = "tomato1",
-                                "Blue"= "royalblue2",
-                                "Green" = "seagreen4",
-                                "Purple" = "mediumpurple1")
-            )
-        ),
-        mainPanel(
-            ui <- fluidPage(
-                plotlyOutput("chart2", height = "700px"),
-                p("This scatterplot graphs different variables, 
+  tags$h4("Movie Ratings"),
+  h1("Comparing Ratings to Specific Variables in 2020"),
+  sidebarLayout(
+    sidebarPanel(
+      x_input <- selectInput(
+        "x_var",
+        label = "X Variable",
+        choices = list(
+          tab2_select_values[1],
+          tab2_select_values[2],
+          tab2_select_values[3]
+        )
+      ),
+      y_input <- selectInput(
+        "y_var",
+        label = "Rating Type",
+        choices = list(
+          tab2_select_values[4],
+          tab2_select_values[5]
+        )
+      ),
+      color_input <- selectInput(
+        "color",
+        label = "Color",
+        choices = list(
+          "Red" = "tomato1",
+          "Blue" = "royalblue2",
+          "Green" = "seagreen4",
+          "Purple" = "mediumpurple1"
+        )
+      )
+    ),
+    mainPanel(
+      ui <- fluidPage(
+        plotlyOutput("chart2", height = "700px"),
+        p("This scatterplot graphs different variables,
                   including Age, Runtime, and Genre, against
                   different ratings (either IMDb or Rotten Tomatoes)
                   for movies that were released in the year 2020."),
-                p("While many of the chart options did not seem to have clear
+        p("While many of the chart options did not seem to have clear
                   correlations, some scatterplots showed to have some
-                  sort of pattern to them. In terms of Age rating for 
+                  sort of pattern to them. In terms of Age rating for
                   the movies, they seemed to have no clear pattern
                   between IMDb or Rotten Tomato ratings."),
-                p("Runtime, however showed to have a higher concentration of
+        p("Runtime, however showed to have a higher concentration of
                   ratings (both IMDb and Rotten Tomatoes) between
                   ~60 to ~150 minutes."),
-                p("For Genre, highest and
+        p("For Genre, highest and
                   most amount of ratings were concentrated in
-                  the genres of action, comedy, drama, and 
+                  the genres of action, comedy, drama, and
                   documentaries for Rotten Tomatoes, and for
-                  IMDb ratings, these genres were comedy, 
+                  IMDb ratings, these genres were comedy,
                   documentaries, and Drama")
-            )
-        )
+      )
     )
+  )
 )
 
 ## PAGE 3 ##
@@ -143,46 +184,49 @@ y_options <- colnames(netflix_stock)[2:7]
 
 # Layout for visual page tab that has Y variable and color options
 page_three <- tabPanel(
-    "Netflix Stock Prices",
-    h1("Netflix Stock Prices Visualization"),
-    sidebarLayout(
-        sidebarPanel(
-            p("Price Options"),
-            selectInput(
-                inputId = "chart3_y_var",
-                label = "Y Variable",
-                choices = y_options
-            ),
-            selectInput(
-                "chart3_color",
-                label = "Color",
-                choices =  list("Red" = "tomato1",
-                                "Blue"= "royalblue2",
-                                "Green" = "seagreen4",
-                                "Purple" = "mediumpurple1")
-            )
-        ),
-        mainPanel(
-            plotlyOutput(outputId = "chart3", height = "700px"),
+  tags$h4("Netflix Stock Prices"),
+  h1("Netflix Stock Prices Visualization"),
+  sidebarLayout(
+    sidebarPanel(
+      p("Price Options"),
+      selectInput(
+        inputId = "chart3_y_var",
+        label = "Y Variable",
+        choices = y_options
+      ),
+      selectInput(
+        "chart3_color",
+        label = "Color",
+        choices = list(
+          "Red" = "tomato1",
+          "Blue" = "royalblue2",
+          "Green" = "seagreen4",
+          "Purple" = "mediumpurple1"
         )
+      )
     ),
-    p("This scatter plot graphs the different prices of Netflix stock
+    mainPanel(
+      plotlyOutput(outputId = "chart3", height = "700px"),
+      p("This scatter plot graphs the different prices of Netflix stock
               by year. Because each data is organized by year, we can also
-              visually see not only how stock prices compared from year to 
-              year, but also from day to day and the range of prices in a 
+              visually see not only how stock prices compared from year to
+              year, but also from day to day and the range of prices in a
               give year. We were interested in the stock prices of Netflix
               because we beleive stock prices are a good indication of the
               company's overall popularity and growth; thereby suggesting
               trends in overall media usage.")
+    )
+  ),
+
 )
 
 ## SUMMARY PAGE ##
 summary_page <- tabPanel(
-    "Summary",
-    mainPanel(
-        h1("Summary"),
-        tags$strong("Movie Count Per Genre vs. Year Insights"),
-        p("Our movie count vs. year bar plot allowed us to see the total number 
+  tags$h4("Summary"),
+  mainPanel(
+    h1("Summary"),
+    tags$strong("Movie Count Per Genre vs. Year Insights"),
+    p("Our movie count vs. year bar plot allowed us to see the total number
           of movies in each genre added to Netflix each year. A notable trend we
           can see in the chart is that between 2016-2017, for many genres,
           there is a steep increase in the number of movies added to Netflix.
@@ -190,10 +234,10 @@ summary_page <- tabPanel(
           more popular during this time period, as Netflix created more contracts
           to add these types of movies to their streaming platform. Furthermore,
           we can see that most of the genres have a steadily increasing count
-          as the year increases, which allows us to conclude that Netflix is 
+          as the year increases, which allows us to conclude that Netflix is
           steadily growing its collection of movies and genres."),
-        tags$strong("Stock Price vs. Year Insights"),
-        p("Our year vs. stock price scatter plot revealed a variety of
+    tags$strong("Stock Price vs. Year Insights"),
+    p("Our year vs. stock price scatter plot revealed a variety of
           interesting trends. Firstly, Open, High, Low, and Close stock
           prices reamined under 5 until 2010. After 2010, we see a nearly
           exponential increase in stock prices through 2020, hitting 549
@@ -204,8 +248,8 @@ summary_page <- tabPanel(
           peaks in 2004 and 2011. Overall, this graph collectively indicates
           that Netflix stock's value is increasing exponentially, which
           may further indicate and exponential increase in media consumption."),
-        tags$strong("Movie Ratings Vs Different Variables"),
-        p("Our movie ratings Vs dIfferent variables scatterplot
+    tags$strong("Movie Ratings Vs Different Variables"),
+    p("Our movie ratings Vs dIfferent variables scatterplot
           allowed us to compare and contrast different variables
           and their affect on IMDb and Rotten Tomatoes ratings.
           While not many correlations were obvious in the charts,
@@ -217,31 +261,30 @@ summary_page <- tabPanel(
           ratings in the runtimes of ~60 to ~150 minutes, and the higher
           ratings in both IMDb and Rotten Tomatoes were mainly concentrated
           in runtimes of ~85 minutes to ~100 minutes. This may infer that
-          movies that are too long or too short tend to have rushed or 
+          movies that are too long or too short tend to have rushed or
           too slow of a plot. It may also mean that movies within this
           timeframe (80 to 100 minutes) may be the best for all audiences
           to enjoy. Lastly, the genre variable, the highest number of
-          ratings for both IMDb and Rotten Tomatoes were concentrated in 
+          ratings for both IMDb and Rotten Tomatoes were concentrated in
           Comedy, Documentaries, and Drama. Highest ratings were also
-          mainly in these categories. This may infer a couple of things, 
-          that directors of these specific genres are typically more 
+          mainly in these categories. This may infer a couple of things,
+          that directors of these specific genres are typically more
           skilled at keeping audiences engaged and therefore result
           in higher ratings, or that audiences tend to watch more of
           these genres and enjoy them more, regardless of technicalities
-          evident in the movie. Overall, some variables are 
+          evident in the movie. Overall, some variables are
           shown to influence the number and the quality of ratings of
           movies more than others.
           ")
-        
-    )
+  )
 )
 
 ui <- navbarPage(
-    "Netflix Exploration",
-    theme = shinytheme("united"),
-    intro_page,
-    page_one,
-    page_two,
-    page_three,
-    summary_page
+  "Netflix Exploration",
+  theme = shinytheme("united"),
+  intro_page,
+  page_one,
+  page_two,
+  page_three,
+  summary_page
 )
